c99a6cdc71c224afa0574a08777202ed1bb5f9d4
Shashi08339 <shashitejakasetty@gmail.com>
2025-11-13 12:03:55 +0530

    Delete module2/module2-java-main/moduel2 java directory

--- module2/module2-java-main/moduel2 java/JumpStatement.java
+++
@@ -1,9 +0,0 @@
-public class JumpStatement {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            if (i == 5)
-                continue; // skip 5
-            System.out.println(i);
-        }
-    }
-}
